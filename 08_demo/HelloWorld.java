3e9876e491861c2b930720e887be9f3c28812317
hanchn <jing.yuan@kapeixi.com>
2023-11-14 15:26:22 +0800

    .

---
+++ 08_demo/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    int a = 5;
+    char b = 'a';
+    String c = "hello";
+    boolean d = true;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+  }
+}
