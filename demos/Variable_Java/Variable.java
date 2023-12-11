8e9104850e3422d4b53a2026724c9d4502994f2a
hanchn <jing.yuan@kapeixi.com>
2023-12-11 14:36:21 +0800

    .

---
+++ demos/Variable_Java/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+  public static void main(String[] args) {
+    /**
+     * 这个是一个内置的打印方法
+     * 输出了一段话：Hello World
+     */
+
+    var myWord = "输出一句话";
+    System.out.println(myWord);
+
+    String myWord2 = "我利用String定义了一个变量";
+    System.out.println(myWord2);
+  }
+}
