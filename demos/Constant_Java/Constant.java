b514bc71f1a370c5695b5399f135f462fd39576f
hanchn <jing.yuan@kapeixi.com>
2023-12-11 14:51:38 +0800

    .

---
+++ demos/Constant_Java/Constant.java
@@ -0,0 +1,9 @@
+public class Constant {
+  public static void main(String[] args) {
+    String word = "Hello World !";
+    word = "1111";
+    final String word2 = "Hello World hahah!";
+    word2 = "2222";
+    System.out.println(word2);
+  }
+}
