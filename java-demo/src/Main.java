612c06c0aed54b67f1663dada95e548e131359f8
hanchn <jing.yuan@kapeixi.com>
2023-12-11 11:03:31 +0800

    .

--- java-demo/src/Main.java
+++ java-demo/src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
+    /**
+     * 注释
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
--- java-demo/src/demos/Demo02.java
+++ java-demo/src/demos/Demo02.java
@@ -1,7 +1,13 @@
 package demos;
 
+import java.util.Scanner;
+
 public class Demo02 {
-    public static  void main(String [] args){
-        System.out.println("Hello World !");
+    public static int [] age = { 1, 2, 3};
+    public static void main(String [] args){
+        System.out.println(age);
+        for (int i = 0 ; i < age.length; i++){
+            System.out.println(age[i]);
+        }
     }
 }
