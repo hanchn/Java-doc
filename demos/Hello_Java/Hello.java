a3f1220321e4233f342123e84b9d8c4774626b31
hanchn <jing.yuan@kapeixi.com>
2023-12-27 17:26:01 +0800

    .

--- demos/Hello_Java/Hello.java
+++ demos/Hello_Java/Hello.java
@@ -1,5 +1,13 @@
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
+
+    test("test");
+    test(1);
   }
+
+  public static <T> T test(T val) {
+    return val;
+  }
+
 }
